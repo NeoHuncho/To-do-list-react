24fdafde9b65563253e7ee6e773b7e4b2cb3682c
NeoHuncho <william.guinaudie@gmail.com>
2020-09-01 17:44:46 +0200

    added forms component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import './App.css';
+import Forms from './components/Forms'
 
 function App() {
   return (
     <div className="App">
       <header>
-        
+        <h1>Will's Todo List</h1>
+        <Forms />
       </header>
     </div>
   );
