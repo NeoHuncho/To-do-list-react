1d6c9cfed405b947ad43aee6478f8d1a3d9a7fa9
NeoHuncho <william.guinaudie@gmail.com>
2020-09-07 17:22:19 +0200

    added the header layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
+import Todos from './components/ToDos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import uuid from 'uuid';
